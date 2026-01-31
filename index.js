f050f53950a7c8c368bba41db85e30ef046f6c6e
ryadevx <ryadevx@gmail.com>
2026-01-31 13:56:51 +0100

    fr

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const { Client, GatewayIntentBits } = require('discord.js');
-const config = require('.src/config/config');
+const config = require('./src/config/config');
 const ReminderService = require('.src/services/reminderService');
 const CommandHandler = require('./src/handlers/commandHandler');
 const { initRedis } = require('./src/config/redis');
